0db18317355740bd60ef450a83f9dc0431f6cc89
gouharlali <gouhar.lali@hotmail.com>
2022-05-11 18:32:28 +0100

    Initial commit

---
+++ app/src/main/java/com/gouhar/breakingbadapp/MainActivity.kt
@@ -0,0 +1,97 @@
+package com.gouhar.breakingbadapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.Person
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.gouhar.breakingbadapp.ui.theme.BreakingBadAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContent {
+            BreakingBadAppTheme {
+
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    Column(
+                        modifier = Modifier.padding(horizontal = 32.dp,
+                            vertical = 8.dp),
+                        verticalArrangement = Arrangement.spacedBy(16.dp)
+                    ) {
+                        CharacterCard(Color.Red, "Johnny Depp")
+                        CharacterCard(Color.Green, "Angelina Jolie")
+                        CharacterCard(Color.Magenta, "Margot Robbie")
+                    }
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun CharacterCard(color:Color, actorName: String){
+    Card(
+        elevation = 10.dp,
+        modifier = Modifier.fillMaxWidth()
+    ){
+        Row (modifier = Modifier
+            .padding(16.dp),
+            horizontalArrangement = Arrangement.spacedBy(16.dp),
+            verticalAlignment = Alignment.CenterVertically) {
+            Box(
+                modifier = Modifier
+                    .clip(CircleShape)
+                    .background(color = color)
+                    .size(70.dp)
+            ) {
+                Icon(
+                    modifier = Modifier
+                        .size(50.dp)
+                        .align(Alignment.Center),
+                    imageVector = (Icons.Rounded.Person),
+                    contentDescription = "",
+                    tint = Color.White
+                )
+            }
+            Text(
+                modifier = Modifier
+                    .align(alignment = Alignment.CenterVertically),
+                text = actorName
+            )
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun CharacterCardPreview() {
+    BreakingBadAppTheme() {
+        Column(
+            modifier = Modifier.padding(horizontal = 32.dp,
+                vertical = 8.dp),
+            verticalArrangement = Arrangement.spacedBy(16.dp)
+        ) {
+            CharacterCard(Color.Red, "Johnny Depp")
+            CharacterCard(Color.Green, "Angelina Jolie")
+            CharacterCard(Color.Magenta, "Margot Robbie")
+        }
+    }
+}
+
---
+++ app/src/main/java/mainscreen/CharacterListViewModel.kt
@@ -0,0 +1,6 @@
+package mainscreen
+
+import androidx.lifecycle.ViewModel
+
+class CharacterListViewModel(): ViewModel() {
+}
